32b66ad461ed91b4067ad5208dd81c559f450dca
Vivian Nguyen <nguy.vivian.vi@gmail.com>
2020-11-21 15:11:39 -0500

    add downloading function for json

--- dataloading.py
+++ dataloading.py
@@ -1,3 +1,33 @@
 '''
 placeholder for loading the data from SQL into memory on deployment
+
+
 '''
+import pandas as pd
+import urllib.request, json 
+
+def data_downloader(name, url):
+    '''
+    Pass in a URL and download the dataframe, save to disk
+    '''
+    # download the json
+    with urllib.request.urlopen(url) as f:
+        data = json.loads(f.read().decode())
+    
+    # save the json to disk
+    filename = f'data/{name}.json'
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+    
+    print(f'Saved {name} to Disk')
+    
+def download_bulk():
+    '''
+    Script to download all jsons to disk 
+    
+    TODO: add some kind of loading bar to show percentage of downloaded
+    '''
+    import dataloading
+    import city_of_london_api_endpoints as col
+    
+    return [dataloading.data_downloader(i, col.EVERYTHING.get(i)) for i in list(col.EVERYTHING.keys())]
